a72ed95a8af1b5afbf8e0e4051945b11f625da1c
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2024-12-28 18:46:50 +0530

    user joined functionaly has added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,93 @@
-import React, { useEffect, useState } from "react" ;
-import io from "socket.io-client" ;
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 const socket = io("http://localhost:5000");
 
 const App = () => {
-  
-  const [message, setMessage]   = useState("");
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const [notifications, setNotifications] = useState([]);
 
-  console.log("render outside") ; 
+  // console.log("render outside") ;
 
   useEffect(() => {
-    //msg is the message that is sent by the server  ... 
+    //msg is the message that is sent by the server  ...
+
     socket.on("chat-message", (msg) => {
-      console.log("message sent  ...") ; 
+      console.log("message sent  ...");
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
+
+    socket.on("user-joined", (notification) => {
+      setNotifications((prevNotification) => [
+        ...prevNotification,
+        notification,
+      ]);
+    });
+
+    socket.on("user-left", (notification) => {
+      setNotifications((prevNotification) => [
+        ...notification,
+        prevNotification,
+      ]);
+    });
+
     return () => {
       socket.off("chat-message");
+      socket.off("user-joined");
+      socket.off("user-left");
     };
   }, []);
+
+  const handleJoin = () => {
+    if (username.trim()) {
+      socket.emit("join", username);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (message.trim()) {
-      //send the message to the server ... 
-      socket.emit('chat-message', message);  
-      setMessage('');
+      //send the message to the server ...
+      socket.emit("chat-message", message);
+      setMessage("");
     }
   };
 
   return (
     <div className="App">
       <h1> real time chat </h1>
+
+      {/* handle the user joined */}
+
+      <div>
+        <input
+          type="text"
+          placeholder="enter the username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={handleJoin}> join chat </button>
+      </div>
+
+      {/* handle the notifications part ..  */}
+      <div>
+        <h2>Notification</h2>
+        <ul>
+          {notifications.map((notificaion, index) => {
+            return <li key={index}> {notificaion} </li>;
+          })}
+        </ul>
+      </div>
+
+      {/* chat message . . */}
+
       <ul>
         {messages.map((msg, index) => {
           return <li key={index}> {msg} </li>;
         })}
       </ul>
+
       <form onSubmit={handleSubmit}>
         <input
           type="text"
